02b1486a5233195ae6d7a2e9eec80d118c6b88b8
Fabricio Enric Smith Freire Silva <37123545+fabricioenric@users.noreply.github.com>
2018-03-19 20:57:17 -0300

    Update funcoes.hpp

--- funcoes.hpp
+++ funcoes.hpp
@@ -1,10 +1,12 @@
 #ifndef FUNCOES_HPP_INCLUDED
 #define FUNCOES_HPP_INCLUDED
 
-int tempo_em_anos(double salario,double despesas); //FunÁ„o para descobrir quantos anos ser„o necess·rios para alguÈm se tornar milion·rio, de acordo com o ganho e o gasto mensal.
+//Protoripo das funcoes:
 
-void numero_de_vezes(int valores[10]); //FunÁ„o para quantificar a frequÍncia de 10 valores inteiros.
+int tempo_em_anos(double salario,double despesas); //Fun√ß√£o para descobrir quantos anos ser√£o necess√°rios para algu√©m se tornar milion√°rio, de acordo com o ganho e o gasto mensal.
 
-void decomposicao_em_notas(int reais); //FunÁ„o para decompor um determinado valor inteiro, em reais, em notas de 100, 50, 20, 10, 5, 2, e 1.
+void numero_de_vezes(int valores[10]); //Fun√ß√£o para quantificar a frequ√™ncia de 10 valores inteiros.
+
+void decomposicao_em_notas(int reais); //Fun√ß√£o para decompor um determinado valor inteiro, em reais, em notas de 100, 50, 20, 10, 5, 2, e 1.
 
 #endif // FUNCOES_HPP_INCLUDED
