2f6d82adefc0cc860480bb5b9675067a70edd8ad
Fabricio Enric Smith Freire Silva <37123545+fabricioenric@users.noreply.github.com>
2018-03-20 08:38:46 -0300

    Update funcoes.hpp

--- funcoes.hpp
+++ funcoes.hpp
@@ -7,6 +7,10 @@ int tempo_em_anos(double salario,double despesas); //Função para descobrir qua
 
 void numero_de_vezes(int valores[10]); //Função para quantificar a frequência de 10 valores inteiros.
 
+struct numero_relatorio{ //Struct para auxiliar na enumeracao dos numeros digitados e quantificar quantas vezes cada um apareceu.
+    int numero,vezes;
+};
+
 void decomposicao_em_notas(int reais); //Função para decompor um determinado valor inteiro, em reais, em notas de 100, 50, 20, 10, 5, 2, e 1.
 
 #endif // FUNCOES_HPP_INCLUDED
